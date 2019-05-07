33528bf715112e47ca13e83bf29932e73f7ca935
Milan Cifra <milan3362@gmail.com>
2019-05-07 20:55:37 +0200

    sparql-transformer testing

--- routes.js
+++ routes.js
@@ -1,9 +1,13 @@
+
+const sparqlTransformer = require('sparql-transformer');
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
-
 const {
   Client
 } = require('virtuoso-sparql-client');
+
+const JSONLD_QUERIES = './json-ld-queries/';
 
 router.get('/', (req, res) => {
   res.render('index');
@@ -13,8 +17,7 @@ router.post('/local', (req, res) => {
   const graphName = req.body.graphName;
   const query = req.body.query;
   const format = req.body.format;
-
-  const localClient = new Client('http://localhost:8890/sparql');
+  localClient = new Client('http://matfyz.sk:8890/sparql');
   localClient.setQueryFormat(format);
   localClient.setQueryGraph(graphName);
 
@@ -29,6 +32,21 @@ router.post('/local', (req, res) => {
       console.log(err);
       res.send('Error!');
     });
+});
+
+router.get('/transformer', (req, res) => {
+  var queries = fs.readdirSync(JSONLD_QUERIES);
+  res.render('transformer', {files: queries});
+});
+
+router.post('/transformer-run', async (req, res) => {
+  const fileName = req.body.queryFile;
+  const q = JSON.parse(fs.readFileSync(`${JSONLD_QUERIES}/${fileName}`, 'utf8'));
+  const out = await sparqlTransformer.default(q);
+  res.render('results', {
+    results: out,
+    format: 'application/json'
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const path = require('path');
 const express = require('express');
 const layout = require('express-layout');
