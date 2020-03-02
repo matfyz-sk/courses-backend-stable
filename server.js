90d9ec4f88ecdd12c25468bc626a3f72235ff764
Milan Cifra <milan3362@gmail.com>
2020-03-02 15:23:44 +0100

    cors

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ import router from "./routes/data";
 import authRouter from "./routes/auth";
 import { authSecret } from "./constants";
 var expressJWT = require("express-jwt");
+import cors from "cors";
 
 const app = express();
 const port = 3010;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(
+    cors({
+        origin: "*"
+    })
+);
 
 app.use("/data", /*expressJWT({ secret: authSecret }),*/ router);
 app.use("/auth", authRouter);
