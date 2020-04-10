9e67e5c88dc39a70b39da712dedb7ff371429cc8
Milan Cifra <milan3362@gmail.com>
2020-04-10 20:35:19 +0200

    structure

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,16 @@ FROM node:latest
 
 WORKDIR /usr/src/app
 
-COPY package*.json ./
+COPY package.json /usr/src/app
 
 RUN npm install
 
-COPY . .
+COPY src /usr/src/app/src
+
+RUN npm run build
+
+RUN npm run prune --production
 
 EXPOSE 3010
 
-CMD ["npm", "start"]
+CMD ["npm", "prod"]
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,16 +16,18 @@ export function checkValidation(req, res, next) {
 }
 
 export function emailIsFree(req, res, next) {
-   runQuery(userProfile, { email: req.body.user.email })
-      .then(data => {
-         if (data["@graph"].length != 0) {
-            return res.status(200).send({ status: false, msg: "Email is already registered!", user: null });
-         }
-         next();
-      })
-      .catch(err => {
-         res.status(500).send(err);
-      });
+   const data = runQuery(userProfile, { email: req.body.user.email });
+
+   if (data["@graph"].length != 0) {
+      return res
+         .status(200)
+         .send({ status: false, msg: "Email is already registered!", user: null });
+   }
+   next();
+
+   // .catch(err => {
+   //    res.status(500).send(err);
+   // });
 }
 
 export async function createUser(req, res) {
@@ -50,19 +52,21 @@ export async function createUser(req, res) {
 
    user
       .store()
-      .then(data => {
+      .then((data) => {
          let token = generateToken({ userURI: user.subject.iri, email: req.body.user.email });
          res.send({
             status: true,
             user: {
-               name: req.body.privacy.use_nickname ? req.body.privacy.nickname : req.body.user.first_name + " " + req.body.user.last_name,
+               name: req.body.privacy.use_nickname
+                  ? req.body.privacy.nickname
+                  : req.body.user.first_name + " " + req.body.user.last_name,
                type: "student",
-               avatar: null
+               avatar: null,
             },
-            _token: token
+            _token: token,
          });
       })
-      .catch(err => {
+      .catch((err) => {
          console.log(err);
          return res.status(500).send({ status: false });
       });
@@ -70,38 +74,40 @@ export async function createUser(req, res) {
 
 function generateToken({ userURI, email }) {
    let token = jwt.sign({ userURI, email }, authSecret, {
-      algorithm: "HS256"
+      algorithm: "HS256",
    });
    return token;
 }
 
 export function login(req, res) {
    runQuery(userProfile, { email: req.body.email })
-      .then(data => {
+      .then((data) => {
          if (data["@graph"].length == 0) {
             return res.status(200).send({
                status: false,
-               msg: "Credentials not valid"
+               msg: "Credentials not valid",
             });
          }
          const userData = data["@graph"][0];
          if (!bcrypt.compareSync(req.body.password, userData.password)) {
             return res.status(200).send({
                status: false,
-               msg: "Credentials not valid"
+               msg: "Credentials not valid",
             });
          }
          res.send({
             status: true,
             user: {
-               name: userData.useNickName ? userData.nickname : userData.firstName + " " + userData.lastName,
+               name: userData.useNickName
+                  ? userData.nickname
+                  : userData.firstName + " " + userData.lastName,
                type: "student",
-               avatar: null
+               avatar: null,
             },
-            _token: generateToken({ userURI: userData["@id"], email: userData.email })
+            _token: generateToken({ userURI: userData["@id"], email: userData.email }),
          });
       })
-      .catch(err => {
+      .catch((err) => {
          console.log(err);
          res.status(500).send({ status: false, msg: err });
       });
@@ -114,43 +120,45 @@ export function githubLogin(req, res) {
       .post("https://github.com/login/oauth/access_token", {
          client_id: "f937b5e763fd295e11b9",
          client_secret: "b0cb4f40f0065a50f5ab671089de7208cf592614",
-         code
+         code,
       })
-      .then(resp => {
+      .then((resp) => {
          const access_token = resp.data.split("&")[0].split("=")[1];
          console.log("ACCESS TOKEN", access_token);
          return axios.get(`https://api.github.com/user?access_token=${access_token}`);
       })
-      .then(resp => {
+      .then((resp) => {
          const email = resp.email;
          console.log("USER EMAIL", email);
          if (!email) {
             return res.status(200).send({
                status: false,
-               msg: "Empty email"
+               msg: "Empty email",
             });
          }
          return runQuery(userProfile, { email });
       })
-      .then(data => {
+      .then((data) => {
          if (data["@graph"].length == 0) {
             return res.status(200).send({
                status: false,
-               msg: "Credentials not valid"
+               msg: "Credentials not valid",
             });
          }
          const userData = data["@graph"][0];
          res.send({
             status: true,
             user: {
-               name: userData.useNickName ? userData.nickname : userData.firstName + " " + userData.lastName,
+               name: userData.useNickName
+                  ? userData.nickname
+                  : userData.firstName + " " + userData.lastName,
                type: "student",
-               avatar: null
+               avatar: null,
             },
-            _token: generateToken(userData)
+            _token: generateToken(userData),
          });
       })
-      .catch(err => {
+      .catch((err) => {
          console.log(err);
          res.status(500).send({ status: false, msg: err });
       });
@@ -161,7 +169,7 @@ export async function patchUser(req, res) {
    user.setSubject(req.user.userURI);
 
    const data = await user.fetch();
-   user.fill(data);
+   user._fill(data);
 
    for (var predicateName in req.body) {
       if (req.body.hasOwnProperty(predicateName)) {
@@ -175,44 +183,26 @@ export async function patchUser(req, res) {
 
    user
       .patch()
-      .then(data => res.status(200).send({ status: true }))
-      .catch(err => res.status(500).send({ status: false, msg: err }));
+      .then((data) => res.status(200).send({ status: true }))
+      .catch((err) => res.status(500).send({ status: false, msg: err }));
 }
 
 export const registrationValidate = [
    body("user").exists(),
-   body("user.first_name")
-      .exists()
-      .isString(),
-   body("user.last_name")
-      .exists()
-      .isString(),
-   body("user.email")
-      .exists()
-      .isEmail(),
-   body("user.password")
-      .exists()
-      .isString(),
+   body("user.first_name").exists().isString(),
+   body("user.last_name").exists().isString(),
+   body("user.email").exists().isEmail(),
+   body("user.password").exists().isString(),
    body("privacy").exists(),
-   body("privacy.use_nickname")
-      .exists()
-      .isBoolean(),
-   body("privacy.public_profile")
-      .exists()
-      .isBoolean(),
-   body("privacy.show_courses")
-      .exists()
-      .isBoolean(),
-   body("privacy.show_badges")
-      .exists()
-      .isBoolean(),
-   body("privacy.allow_contact")
-      .exists()
-      .isBoolean(),
+   body("privacy.use_nickname").exists().isBoolean(),
+   body("privacy.public_profile").exists().isBoolean(),
+   body("privacy.show_courses").exists().isBoolean(),
+   body("privacy.show_badges").exists().isBoolean(),
+   body("privacy.allow_contact").exists().isBoolean(),
    body("privacy.nickname")
-      .if(body("privacy.use_nickname").custom(value => value === true))
+      .if(body("privacy.use_nickname").custom((value) => value === true))
       .exists()
-      .isString()
+      .isString(),
 ];
 
 export const loginValidate = [body("email").exists(), body("password").exists()];
