7726cd197b359b3e7e423062c5d46aa7299d822a
Milan Cifra <milan3362@gmail.com>
2020-04-18 13:25:09 +0200

    model

--- src/model/event/block.js
+++ src/model/event/block.js
@@ -3,5 +3,12 @@ import { event } from "./event";
 export const block = {
    type: "block",
    subclassOf: event,
-   props: {}
+   props: {
+      courseInsntace: {
+         required: true,
+         multiple: false,
+         dataType: "node",
+         objectClass: "courseInstance",
+      },
+   },
 };
