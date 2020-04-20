c2bfb20e5cd4b791be25e069402a7162af012ea0
adam.grund@gmail.com <adam.grund@gmail.com>
2020-04-20 10:28:03 +0200

    change port for stable

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const authSecret = "courses";
-export const virtuosoEndpoint = "http://matfyz.sk:8890/sparql";
+export const virtuosoEndpoint = "http://matfyz.sk:8891/sparql";
 export const dcTermsURI = "http://purl.org/dc/terms/";
 export const ontologyURI = "http://www.courses.matfyz.sk/ontology#";
 export const graphURI = "http://www.courses.matfyz.sk/data";
